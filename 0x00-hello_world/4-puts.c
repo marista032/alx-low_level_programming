0bd74c5f51567502c5cd753057e66511cc086792
marista032 <marista032@gmail.com>
2022-09-08 16:08:53 -0700

    task 4 hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stuio.h>
+
+/**
+*main - print the string in the put function
+*
+*Description: using the main function
+*ths program prints "Programming is like building a multilingual puzzle"
+*Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+}
