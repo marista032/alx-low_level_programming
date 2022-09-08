cd85e4ea5dd9323ac86f37784f6138bdc152a4f0
marista032 <marista032@gmail.com>
2022-09-08 16:37:03 -0700

    task 5 hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stuio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program priints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n);
+	return(0);
+}
