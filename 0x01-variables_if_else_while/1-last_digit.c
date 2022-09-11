ed730335df43206022acb89300d9d56ec4f8c898
marista032 <marista032@gmail.com>
2022-09-11 06:25:21 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ else if (l == 0)
 }
 else
 {
-	printf("Last digit of %d and is less than 6 and not 0\n", n, l);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
