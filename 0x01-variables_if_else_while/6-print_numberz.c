fd6af97deefaf5958f91717fb5f3ae9217ab840c
marista032 <marista032@gmail.com>
2022-09-11 09:13:52 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,6 +10,11 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+char c;
+for (c = 'z'; c <= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
