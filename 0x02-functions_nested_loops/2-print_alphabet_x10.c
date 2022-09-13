7156fc67f86edb21931d820940616d0474d7f8b9
marista032 <marista032@gmail.com>
2022-09-13 10:35:05 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char c, i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
