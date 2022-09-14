0e67c8a7c5f0a55706b8fb36ee8789ee1844ff9a
marista032 <marista032@gmail.com>
2022-09-14 10:47:00 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the time tble
+ * @n: integer for which the time table will printed
+ *
+ * Description: print the times table
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+int row, column, product;
+
+if (n >= 0 && n < 15)
+{
+for (row = 0; row <= n; row++)
+{
+for (column = 0; column <= n; column++)
+{
+product = row * column;
+
+if (column == 0)
+_putchar('0');
+else if (product < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((product / 10) % 10 + '0');
+_putchar(product % 10 + '0');
+}
+else if (product > 99 && product < 1000)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(product / 100 + '0');
+_putchar((product / 10) % + '0');
+_putchar(product % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
+}
