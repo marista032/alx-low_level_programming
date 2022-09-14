fbb9777625e715b1368e588488b1749cf1042de9
marista032 <marista032@gmail.com>
2022-09-14 05:00:19 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print _sign - function to check for a sign of a number
- * @c: is the int that use for the argument of the function
+ * print_sign - function to check for a sign of a number
+ * @n: is the int that use for the argument of the function
  * Return: 0
  */
 int print_sign(int n);
