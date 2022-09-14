22e17b359818c199f17e7987e330d7c92007effe
marista032 <marista032@gmail.com>
2022-09-14 15:45:18 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints out first digit 50
+ * fibonacci suit numbers
+ * Return: return 0
+ */
+int main;
+unsigned integer n1 = 0, n2 = 1, n3;
+for (digit = 0; digits < 50; digits++)
+{
+n3 = n1 + n2;
+printf("%lu", n3);
+n1 = n2;
+n2 = n3;
+
+if  (inc == 49)
+printf("\n");
+else
+printf(",");
+
+}
+
+return (0);
