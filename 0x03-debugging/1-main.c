e3200697312bba93261245cbf5c7b813d2fcc9f4
marista032 <marista032@gmail.com>
2022-09-15 07:44:25 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-*main - causes an infinite loop
-*Return: 0
+* main - causes an infinite loop
+* Return: 0
 */
 
 int main(void)
@@ -18,7 +18,7 @@ i = 0;
 *putchar(i);
 *}
 */
-printf("Infinite loop avoided! \\o/n");
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
