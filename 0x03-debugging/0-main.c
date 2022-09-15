3b3d75f7b5ddd0ec084fc4f8f05040c3b893f1bc
marista032 <marista032@gmail.com>
2022-09-15 05:06:04 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -13,4 +13,3 @@ i = 0;
 positive_or_negative(i);
 
 return (0);
-}
