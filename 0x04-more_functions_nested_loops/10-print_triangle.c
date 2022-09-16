6efbd768193b2c507ac7685c5ae16438868a7137
marista032 <marista032@gmail.com>
2022-09-15 21:42:23 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -20,18 +20,16 @@ void print_triangle(int size)
 				putchar(' ');
 			}
 
-			
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
 				putchar('#');
 			}
 
-			
 			if (inc1 == size)
 			{
 				continue;
 			}
-			
+
 			putchar('\n');
 		}
 	}
