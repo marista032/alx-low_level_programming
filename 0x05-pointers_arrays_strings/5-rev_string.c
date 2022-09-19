d8547b0cb415ebdac62639f317a3dfda4dd7dcd4
marista032 <marista032@gmail.com>
2022-09-19 14:03:10 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,7 +4,6 @@
 /**
 * rev_string - Reverses a string
 * @s: string to be reversed
-*
 * Return: nothing
 */
 
@@ -22,15 +21,15 @@ void rev_string(char *s)
 
 /**
 * _strlen - returns the length of a string
-* @s: string
-*
 * Return: the length of the given string
 */
 
 int _strlen(char *s)
 {
+	int len = 0;
+
 	while (*(s + len) != '\0')
 		len++;
-	
+
 	return (len);
 }
