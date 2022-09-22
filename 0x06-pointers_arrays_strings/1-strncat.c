ec100f8394c98bc68328766f851a8b5b8418fd67
marista032 <marista032@gmail.com>
2022-09-22 02:00:53 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_strncat - concatenate two strings but add imputted number of bytes
+*_strncat - concatenate two strings but add inputted number of bytes
 *@dest: string to be appended upon
 *@src: string to be completed at the end of dest
 *@n:integer parameter to compare index to
@@ -9,13 +9,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		est_len++;
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
+
 }
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,3 @@
+#include "main.h"
+/***_strncpy - function that copies a string including the
+* thermonating null byte, using at most an imptted number of bytes
