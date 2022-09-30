b242b88740d79edfe4fb70a180fc033d0fadf5de
marista032 <marista032@gmail.com>
2022-09-30 13:36:10 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+I
 
 /**
- * main - prints all arguments it recieves
- * @agrc: n args
- * @argv: arr args
+ * main - a program that prints all arguments it recieves
+ * @agrc: argument counts
+ * @argv: array of pointers to argument string
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	while (agrc--)
+		printf("%s\n", argv++);
 	return (0);
 }
