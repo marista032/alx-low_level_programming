bbebd2027def4dddf34e136559483ef3e5120c4e
marista032 <marista032@gmail.com>
2022-10-12 15:20:06 -0700

    task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+#include <stdlib.h>
+
+#include <stddef.h>
+
+int_putchar(char);
+
+void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /*_FUNCTION_POINTERS_H_*/
