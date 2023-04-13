c948113889e8714a1090abcc0dc2ed94c17cc2af
marista032 <marista032@gmail.com>
2023-04-13 09:50:36 -0700

    commits for tasks on search_algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,13 +5,10 @@
 #include <math.h>
 
 /**
- * struct listint_s - singly linked list
- *
+ * This script describes the strcture of singly linked lists
  * @n: Integer
  * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
+ * @next: Pointer to the next
  */
 typedef struct listint_s
 {
@@ -21,14 +18,11 @@ typedef struct listint_s
 } listint_t;
 
 /**
- * struct skiplist_s - Singly linked list with an express lane
- *
+ * This script describes the strcture of singly linked list with an express lane
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
  */
 typedef struct skiplist_s
 {
